44d3dc5223776fa02f09633a14d4a9c0a9e29636
Jamie Bowman <jamiereecebowman@gmail.com>
2019-12-11 18:03:47 +0000

    refactor rss plugin using feedjira and httparty

--- Gemfile
+++ Gemfile
@@ -5,8 +5,9 @@ gem "jekyll", "~> 3.4"
 
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
-  gem 'rss', '~> 0.2.8'
-  gem 'byebug', '~> 11.0', '>= 11.0.1'
+  gem 'feedjira', '~> 3.1'
+  gem 'httparty', '~> 0.17.1'
+  gem 'nokogiri', '~> 1.10', '>= 1.10.4'
   gem 'pry', '~> 0.12.2'
   gem 'slugify', '~> 1.0', '>= 1.0.7'
 end
--- _plugins/rss.rb
+++ _plugins/rss.rb
@@ -1,107 +1,77 @@
-require 'rss'
-require 'open-uri'
-require 'byebug'
-require 'pry'
-require 'slugify'
+require "feedjira"
+require "httparty"
+require "nokogiri"
+require "pry"
+require "cgi"
 
-def feed_xml_to_json(feed_url)
-  open(feed_url) do |rss|
-    puts "Requesting data from #{feed_url}..."
-    feed = RSS::Parser.parse(rss)
-    feed_data = Array.new
-    feed_meta = {
-      feed_owner: (feed.channel.itunes_owner.itunes_name rescue nil),
-      feed_title: (feed.channel.title rescue nil),
-      feed_desc: (feed.channel.description rescue nil),
-      feed_lang: (feed.channel.language rescue nil),
-      feed_last_built: (feed.channel.lastBuildDate rescue nil),
-      feed_cvr_img: (feed.channel.itunes_image rescue nil),
-      feed_category: (feed.channel.itunes_category rescue nil)
+def remove_html_tags(str)
+  re = /<("[^"]*"|'[^']*'|[^'">])*>/
+  str.gsub!(re, '')
+end
+
+def parse_feed(url)
+  xml = HTTParty.get(url).body
+  feed = Feedjira.parse(xml)
+  doc = Nokogiri::HTML(HTTParty.get(url).body)
+  puts "Parsing '#{doc.title}' at #{url}..."
+  # binding.pry
+  episodes = Array.new
+  feed.entries.each do |entry|
+    episode = {
+      src: url,
+      url: entry.url,
+      feed: doc.title,
+      title: entry.title.strip,
+      author: entry.itunes_author,
+      pub_date: entry.published,
+      desc: entry.summary,
+      audio: entry.enclosure_url,
+      duration: entry.itunes_duration,
+      length: entry.enclosure_length,
+      type: entry.enclosure_type,
+      thumbnail: entry.itunes_image
     }
-    feed_data << feed_meta
-    episodes = Array.new
-    feed.items.each_with_index do |item, n|
-      episode = {
-        episode_title: (item.title.strip rescue nil),
-        episode_desc: (item.description.strip rescue nil),
-        episode_pub_date: (item.pubDate rescue nil),
-        episode_author: (item.itunes_author rescue nil),
-        episode_duration_unix: (item.enclosure.length rescue nil),
-        episode_duration: (item.itunes_duration.content rescue nil),
-        episode_format: (item.enclosure.type rescue nil),
-        episode_url: (item.enclosure.url rescue nil),
-        episode_keywords: (item.itunes_keywords rescue nil)
-      }
-      episodes << episode
-    end
-    feed_meta.store("episodes", episodes)
-    puts "'#{feed_meta[:feed_title]}' succesfully mapped: #{episodes.length} episodes found"
-    return feed_meta
+    episodes.push(episode)
   end
+  puts "#{episodes.length} episodes found"
+  return episodes
 end
 
 def process_data
-  
-  # create new array to store data from each feed 
-  all_data = Array.new
-  
-  # pass urls thru mapping function and store in array
-  all_data << feed_xml_to_json("http://feeds.soundcloud.com/users/soundcloud:users:13730980/sounds.rss")
-  all_data << feed_xml_to_json("http://feeds.soundcloud.com/users/soundcloud:users:14268499/sounds.rss")
-  all_data << feed_xml_to_json("http://rss.acast.com/vintagepodcast")
-  # all_data << feed_xml_to_json("http://feeds.soundcloud.com/users/soundcloud:users:8180342/sounds.rss")
-  # all_data << feed_xml_to_json("https://www.theguardian.com/books/series/books/podcast.xml")
-  # all_data << feed_xml_to_json("http://feeds.soundcloud.com/users/soundcloud:users:313878848/sounds.rss")
-  # all_data << feed_xml_to_json("http://feeds.poetryfoundation.org/PoetryOffTheShelf?id=510219")
-  # all_data << feed_xml_to_json("https://rss.acast.com/readlikeawriter")
-  # all_data << feed_xml_to_json("http://independentpublishersguild.libsyn.com/rss")
-  # all_data << feed_xml_to_json("https://feed.pippa.io/public/shows/5ce3bd97862700d1704d3dfb")
-  # all_data << feed_xml_to_json("https://feeds.soundcloud.com/users/soundcloud:users:47550828/sounds.rss")
-  # all_data << feed_xml_to_json("http://feeds.soundcloud.com/users/soundcloud:users:535574250/sounds.rss")
-  # all_data << feed_xml_to_json("https://feeds.fireside.fm/verybadwizards/rss")
-  # all_data << feed_xml_to_json("https://rss.acast.com/londonreviewpodcasts")
-  # all_data << feed_xml_to_json("http://feeds.soundcloud.com/users/soundcloud:users:1076867/sounds.rss")
-  
-  # write data to file
-  File.open("./_data/all_data.json", "w") do |file|
-    file.write(JSON.pretty_generate(all_data))
-  end
-  
-  # flatten data array into one big episodes list
-  episodes = all_data.map { |podcast| podcast["episodes"] }.flatten
-  
-  # write episodes data to file
+  data = Array.new
+  data << parse_feed("https://goldenharebooks.com/feed/podcast/")
+  data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:13730980/sounds.rss")
+  # data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:14268499/sounds.rss")
+  # data << parse_feed("https://rss.acast.com/vintagepodcast")
+  # data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:8180342/sounds.rss")
+  # data << parse_feed("https://www.theguardian.com/books/series/books/podcast.xml")
+  # data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:313878848/sounds.rss")
+  # data << parse_feed("https://rss.acast.com/readlikeawriter")
+  # data << parse_feed("https://independentpublishersguild.libsyn.com/rss")
+  # data << parse_feed("https://feed.pippa.io/public/shows/5ce3bd97862700d1704d3dfb")
+  # data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:47550828/sounds.rss")
+  # data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:535574250/sounds.rss")
+  # data << parse_feed("https://feeds.fireside.fm/verybadwizards/rss")
+  # data << parse_feed("https://rss.acast.com/londonreviewpodcasts")
+  # data << parse_feed("https://feeds.soundcloud.com/users/soundcloud:users:1076867/sounds.rss")
+  puts "#{data.length} feeds parsed for #{data.flatten.length} episodes"
   File.open("./_data/episodes.json", "w") do |file|
-    file.write(JSON.pretty_generate(episodes))
-  end
-  
-  # open episodes data and convert each date string to date and rewrite to file
-  json = JSON.parse(File.read("./_data/episodes.json"))
-  json = json.each do |episode|
-    episode["episode_pub_date"] = Date.parse(episode["episode_pub_date"])
-  end
-  File.open("./_data/episodes.json", "w") do |file|
-    file.write(JSON.pretty_generate(json))
-  end
-  
-  # summarize
-  puts "#{all_data.length} podcasts scraped for #{all_data.map { |podcast| podcast['episodes'] }.flatten.length} episodes"
-end
-
-def remap_data
-  json = JSON.parse(File.read("./_data/all_data.json"))
-  json = json.each do |feed|
-    feed["episodes"].each do |episode|
-      episode["episode_pub_date"] = Date.parse(episode["episode_pub_date"])
-      episode["episode_feed_title"] = feed["feed_title"]
-      episode["episode_feed_cvr_img"] = URI.extract(feed["feed_cvr_img"]).last
-    end
-  end
-  json = json.map { |podcast| podcast["episodes"] }.flatten.sort_by { |k| k["episode_pub_date"] }.reverse
-  File.open("./_data/mapped_data.json", "w") do |file|
-    file.write(JSON.pretty_generate(json.take(10)))
+    file.write(JSON.pretty_generate(data.flatten))
   end
 end
 
 process_data
-remap_data
+
+def edit_data(filename)
+  json = JSON.parse(File.read("./_data/#{filename}"))
+  json = json.each do |episode|
+    episode["pub_date"] = Date.parse(episode["pub_date"])
+    episode["length"] = episode["length"].to_i
+    # episode["desc"] = remove_html_tags(episode["desc"])
+  end
+  File.open("./_data/episodes.json", "w") do |file|
+    file.write(JSON.pretty_generate(json))
+  end
+end
+
+edit_data("episodes.json")
